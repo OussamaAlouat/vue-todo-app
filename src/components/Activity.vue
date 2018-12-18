4cc4fda7edda13f88881221c8be8785cf488cb37
OussamaAlouat <al317257@uji.es>
2018-12-18 07:58:59 +0100

    Added the counter of activities

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -2,16 +2,35 @@
     <div class="main">
         <h1>ToDo APP</h1>
 
-        <div  v-for="item in activities"  >
+        <div v-for="item in activities">
             <el-row style="margin-bottom: 0.51rem;" :class="getClass(item)">
                 <el-col :span="8">
                     {{item.name}}
                 </el-col>
                 <el-col :span="8">
-                    <el-button type="danger" icon="el-icon-delete" @click="removeActivity(item)" circle size="small"></el-button>
+                    <el-button type="danger" icon="el-icon-delete" @click="removeActivity(item)" circle
+                               size="small"></el-button>
                 </el-col>
                 <el-col :span="8">
-                    <el-button type="success" icon="el-icon-check" circle @click="completeActivity(item)" size="small"></el-button>
+                    <el-button type="success" icon="el-icon-check" circle @click="completeActivity(item)"
+                               size="small"></el-button>
+                </el-col>
+            </el-row>
+        </div>
+
+        <div>
+            <el-row style="width: 60%; margin-left: 40%; margin-bottom: 0.24rem;" >
+                <el-col :span="3">
+                    Completed :
+                </el-col>
+                <el-col :span="2">
+                    {{completed}}
+                </el-col>
+                <el-col :span="3">
+                    Total :
+                </el-col>
+                <el-col :span="2">
+                    {{total}}
                 </el-col>
             </el-row>
         </div>
@@ -56,7 +75,15 @@
             }),
             isWrongActivity() {
                 return this.wrong
+            },
+            completed() {
+                return this.activities.filter((val) => val.completed === true).length;
+            },
+            total() {
+                return this.activities.length;
             }
+
+
         },
         methods: {
             ...mapActions(['addActivity', 'deleteActivity', 'changeActivityState']),
@@ -79,8 +106,8 @@
             completeActivity(item) {
                 this.changeActivityState({activity: item})
             },
-            getClass (item) {
-                return item.completed=== true ? 'completed': ''
+            getClass(item) {
+                return item.completed === true ? 'completed' : ''
             }
 
         }
@@ -93,7 +120,7 @@
     }
 
     .completed {
-        background: rgba(0,0,0,0.3);
+        background: rgba(0, 0, 0, 0.3);
     }
 
 </style>
