666c628fe2fc4a265115e06f7dbaa5e3a2249387
OussamaAlouat <al317257@uji.es>
2018-12-19 18:36:20 +0100

    Refactored code and improved the error notification

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -3,14 +3,17 @@
         <h1>ToDo APP</h1>
         <div v-for="item in activities">
             <el-row style="margin-bottom: 0.51rem;" :class="getClass(item)">
-                <el-col :span="8">
+                <el-col :span="6">
                     {{item.name}}
                 </el-col>
-                <el-col :span="8">
+                <el-col :span="6">
+                    {{item.date}}
+                </el-col>
+                <el-col :span="6">
                     <el-button type="danger" icon="el-icon-delete" @click="removeActivity(item)" circle
                                size="small"></el-button>
                 </el-col>
-                <el-col :span="8">
+                <el-col :span="6">
                     <el-button type="success" icon="el-icon-check" circle @click="completeActivity(item)"
                                size="small"></el-button>
                 </el-col>
@@ -36,7 +39,7 @@
 
         <div v-if="isWrongActivity" style="margin-bottom: 0.2rem; width: 80%; margin-left: 10%; text-align: center">
             <el-alert
-                    title="The activity && the date are empty"
+                    :title="errorMessage"
                     type="error"
                     :center="true"
                     show-icon
@@ -61,7 +64,7 @@
                 </el-col>
 
                 <el-col :span="8">
-                    <el-input placeholder="Please input the date"
+                    <el-input placeholder="Please input the date: dd/mm/yyyy"
                               v-model="date" size="mini">
                     </el-input>
                 </el-col>
@@ -83,7 +86,8 @@
             return {
                 activity: '',
                 wrong: false,
-                date: ''
+                date: '',
+                errorMessage: ''
             }
         },
         computed: {
@@ -105,27 +109,55 @@
         methods: {
             ...mapActions(['addActivity', 'deleteActivity', 'changeActivityState']),
             addActivityMethod() {
-                if (this.activity !== '' && this.date !== '') {
+                if (this.validateData() === true) {
                     const activity = {
                         name: this.activity,
                         completed: false,
                         date: this.date
                     };
-                    this.wrong = false;
+
                     this.addActivity({activity});
-                    this.activity = ''
+                    this.activity = '';
+                    this.date= '';
+                    this.wrong = false;
+
                 } else {
-                    this.wrong = true
+                    this.wrong = true;
+                    this.setMessageError();
                 }
             },
+
             removeActivity(item) {
                 this.deleteActivity({activity: item})
             },
+
             completeActivity(item) {
                 this.changeActivityState({activity: item})
             },
+
             getClass(item) {
                 return item.completed === true ? 'completed' : ''
+            },
+
+            validateData () {
+                if (this.activity !== '' && this.date !== '') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+            setMessageError() {
+                if (this.activity === '' && this.date === '') {
+                    this.errorMessage= 'The activity && the date are empty';
+                } else {
+                    if (this.activity === '') {
+                        this.errorMessage = 'The activity is empty';
+                    } else {
+                        this.errorMessage = 'The date is empty';
+
+                    }
+                }
             }
 
         }
