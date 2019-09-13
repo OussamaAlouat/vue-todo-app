76a29d81a822857d15503e321e2b33eef133c9ee
oussamaAlouat <al317257@uji.es>
2019-09-13 15:24:59 +0200

    Modify all server.js to work with vue

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const port = process.env.PORT || 8080;
+
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+app.use(express.static(__dirname + "dist"));
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+
 app.listen(port);
+
+console.log('Server start on: ', port)
