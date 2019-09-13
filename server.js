bd527dceb1b47edbdf8718d0e30103c5c4da5780
oussamaAlouat <al317257@uji.es>
2019-09-13 16:03:40 +0200

    Refactored server.js file, to run on heroku

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+
 const port = process.env.PORT || 8080;
 
 app = express();
-app.use(express.static(__dirname + "dist"));
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
-
+app.use(serveStatic(path.join(__dirname, 'dist')));
 app.listen(port);
 
 console.log('Server start on: ', port)
