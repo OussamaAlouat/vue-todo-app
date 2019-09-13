80be9f22b223eff3a7b3dec1d9053f4a069c04b0
oussamaAlouat <al317257@uji.es>
2019-09-13 15:52:30 +0200

    Changes to run on heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 app = express();
 app.use(express.static(__dirname + "dist"));
 app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + "/dist/index.html");
+    res.sendfile(__dirname + "/dist/index.html");
 });
 
 app.listen(port);
