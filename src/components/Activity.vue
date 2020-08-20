c006a7b2adc607391b2011c712dd2c7cebe5039a
OussamaAlouat <al317257@uji.es>
2020-08-20 13:58:21 +0200

    Remove input section code

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -39,39 +39,6 @@
           </el-table-column>
         </el-table>
     </div>
-
-    <div>
-      <el-row>
-        <el-col :span="3">
-          Activity:
-        </el-col>
-
-        <el-col :span="8">
-          <el-input
-            placeholder="Please input the activity"
-            v-model="activity"
-            size="mini">
-          </el-input>
-        </el-col>
-        <el-col :span="3">
-          Date:
-        </el-col>
-        <el-col :span="8">
-          <el-date-picker
-            v-model="date"
-            type="date"
-            placeholder="Pick a day">
-          </el-date-picker>
-        </el-col>
-      </el-row>
-      <el-button
-        type="primary"
-        icon="el-icon-circle-plus-outline"
-        circle
-        @click="addActivityMethod()">
-      </el-button>
-    </div>
-
   </div>
 </template>
 
@@ -97,25 +64,6 @@
     },
     methods: {
       ...mapActions(['addActivity', 'deleteActivity', 'changeActivityState', 'setErrorMessageAction']),
-      addActivityMethod() {
-        if (this.validateData() === true) {
-          const activity = {
-            name: this.activity,
-            completed: false,
-            date: this.date,
-            id: uuid()
-          };
-          this.setErrorMessageAction({message: ''});
-
-          this.addActivity({activity});
-          this.activity = '';
-          this.date = '';
-
-        } else {
-          this.setMessageError();
-        }
-      },
-
       completeActivity(item) {
         this.changeActivityState({activity: item})
       },
@@ -128,30 +76,6 @@
         }
 
         return '';
-      },
-
-      validateData() {
-        if (this.activity !== '' && this.date !== '') {
-          return true;
-        } else {
-          return false;
-        }
-      },
-
-      setMessageError() {
-        let error = '';
-        if (this.activity === '' && this.date === '') {
-          error = 'The activity && the date are empty';
-
-        } else {
-          if (this.activity === '') {
-            error = 'The activity is empty';
-          } else {
-            error = 'The date is empty';
-          }
-        }
-
-        this.setErrorMessageAction({message: error});
       },
 
       getDate (item) {
